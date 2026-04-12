3ceb402db07015e2528e22c09b28755207046937
Likhin M N <mnlikhin@gmail.com>
2026-04-12 19:31:18 +0530

    add GitHub scraping functions to retrieve repository URLs and README contents

--- src/pyrove/scrapers/github.py
+++ src/pyrove/scrapers/github.py
@@ -1,4 +1,21 @@
 """github scraper module of the pyrove package"""
-import os
 import httpx
-import asyncio
+from typing import List
+
+def search_github(topic:str)->List[str]:
+    topic=topic.replace(" ", "+")
+    res=httpx.get(f"https://api.github.com/search/repositories?q={topic}&sort=stars&per_page=10",headers={"Accept": "application/vnd.github+json"}).json()
+    urls=[item['html_url'] for item in res['items']]
+    return urls
+
+def scrape_readme(url:str)->str:
+    res=httpx.get(url.replace("github.com", "raw.githubusercontent.com") + "/master/README.md")
+    if res.status_code==404:
+        res=httpx.get(url.replace("github.com", "raw.githubusercontent.com") + "/main/README.md")
+    return res.text
+
+def scrape_all(urls:List[str])->List[str]:
+    contents=[]
+    for url in urls:
+        contents.append(scrape_readme(url))
+    return contents
